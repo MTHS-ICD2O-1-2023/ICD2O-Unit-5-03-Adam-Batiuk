b38f782461639efca11a6c1fac7053194e9edb0f
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-05 15:49:46 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adam Batiuk
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // input
+  const age = parseInt(document.getElementById("user-age").value)
+  // process
+  if (age >= 17) {
+    document.getElementById("answer").innerHTML = "You can see a R rated movie alone."
+  } else if (age >= 13) {
+    document.getElementById("answer").innerHTML = "You can see a PG-13 rated movie alone."
+  } else if (age >= 5) {
+    document.getElementById("answer").innerHTML = "You can see a G or PG rated movie alone."
+  } else {
+    document.getElementById("answer").innerHTML = "You are too young for most things."
+  }
+}
